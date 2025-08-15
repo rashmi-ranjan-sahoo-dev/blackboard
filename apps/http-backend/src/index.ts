8710b3d51abf47d5ae171f068a6a1127a6cf0b2b
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-08-15 19:53:17 +0530

    inital

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+
 
 const app = express();
 
 app.post("/signup",(req,res) =>{
+
+    // db call
+
+    res.json({
+        userId: "123"
+    })
 
 })
 
@@ -12,13 +20,19 @@ app.post("/signin",(req,res) =>{
     
 
     const userId = 1;
-    jwt.sign({
+  const token = jwt.sign({
         userId
-    },JWT_SECRET)
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
 })
 
-app.post("/room",(req,res) =>{
-    
+app.post("/room",middleware,(req,res) =>{
+    //db call
+
+    res.json({ roomId: '123'})
 })
 
 app.listen(3001);
