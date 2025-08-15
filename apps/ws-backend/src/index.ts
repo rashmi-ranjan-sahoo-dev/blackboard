51999eefa26ac486dd68e24b79e81405cae47f23
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-08-15 21:09:55 +0530

    add common backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -16,7 +16,12 @@ wss.on('connection', function connection(ws, request) {
   const token = queryParams.get('token') || "";
   const decoded = jwt.verify(token,JWT_SECRET)
 
-  if(!decoded || !(decoded as JwtPayload).userId) {
+  if(typeof decoded == "string"){
+    ws.close();
+    return;
+  }
+
+  if(!decoded || !decoded.userId) {
     ws.close();
     return;
   }
