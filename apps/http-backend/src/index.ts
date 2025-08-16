9ba18abbc52abc433e4336bd1e3e65c886a15572
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-08-16 22:53:35 +0530

    first class completed

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,11 +2,18 @@ import express from "express";
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { middleware } from "./middleware";
-
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from"@repo/common/types"
 
 const app = express();
 
 app.post("/signup",(req,res) =>{
+
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message: "incorrect inputs"
+        })
+    }
 
     // db call
 
@@ -18,6 +25,12 @@ app.post("/signup",(req,res) =>{
 
 app.post("/signin",(req,res) =>{
     
+  const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message: "incorrect inputs"
+        })
+    }
 
     const userId = 1;
   const token = jwt.sign({
@@ -30,6 +43,15 @@ app.post("/signin",(req,res) =>{
 })
 
 app.post("/room",middleware,(req,res) =>{
+
+
+      const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message: "incorrect inputs"
+        })
+    }
+
     //db call
 
     res.json({ roomId: '123'})
