cd8f133e0c91fffc2a3fcd600ba2aa947e08fa0d
rashmi-ranjan-sahoo-dev <rinkuasahoo04@gmail.com>
2025-08-21 19:04:25 +0530

    backend continiue

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,17 +2,40 @@
 
 import { useState } from "react";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
   const [roomId, setRoomId] = useState("");
-
+  
+  const router = useRouter()
   
   return (
-    <div className={styles.page}>
-      <input value={roomId} onChange={(e) => {
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw"
+    }}>
+    <div>
+      <input
+      style={{
+        padding: 10
+      }}
+      value={roomId} onChange={(e) => {
         setRoomId(e.target.value);
       }} type="text" ></input>
+
+      <button
+      style={{
+        padding: 10
+      }}
+      onClick={() => {
+        router.push(`/room/${roomId}`)
+      }}
+      >Join Room</button>
+    </div>
     </div>
   );
 }
